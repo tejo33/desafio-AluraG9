58a3e2a8b3db1ab978526019f61f2864b9017e81
tejo33 <jlpineda2009@hotmail.com>
2025-07-30 01:40:03 -0500

    pimera version del desafio
    creando el array de amigos

---
+++ app.js
@@ -0,0 +1,4 @@
+function agregarNombreAmigo(){
+    
+
+}// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
