61868d81eadc824197033e2d86075ed0033f8fee
tejo33 <jlpineda2009@hotmail.com>
2025-08-06 08:55:57 -0500

    la lista en la pantalla

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let amigos=[];
+let i=0
 function agregarNombreAmigo(){
      let elementoHTML =document.getElementById('amigo').value;
      if(elementoHTML!==''){
     console.log(elementoHTML);
-    amigos.push(elementoHTML);    
+    amigos.push(elementoHTML);
+    let nombreListA=document.getElementById('listaAmigos');
+    let nuevoNombre =document.createElement('li');
+    nuevoNombre.textContent=elementoHTML;
+    nombreListA.appendChild(nuevoNombre);
+
+    
+
+     
+    
+        
      document.getElementById('amigo').value='';
+     
+     
 
     }else{
         alert("Por favor, inserte un nombre.");
