4839d65b3918b267a721329bff184d185d6b61f2
tejo33 <jlpineda2009@hotmail.com>
2025-08-11 03:16:50 -0500

    funcion para actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 let amigos=[];
-let i=0
+let nombreL='';
+
 function agregarNombreAmigo(){
      let elementoHTML =document.getElementById('amigo').value;
      if(elementoHTML!==''){
-    console.log(elementoHTML);
-    amigos.push(elementoHTML);
-    let nombreListA=document.getElementById('listaAmigos');
-    let nuevoNombre =document.createElement('li');
-    nuevoNombre.textContent=elementoHTML;
-    nombreListA.appendChild(nuevoNombre);
-
     
-
      
-    
-        
-     document.getElementById('amigo').value='';
-     
-     
-
+     for(let i=0 ;i< amigos.length; i++){
+        if(elementoHTML==amigos[i]){
+            alert(`'${elementoHTML}' ya`);           
+            document.getElementById('amigo').value='';
+            return;
+        }
+     }
+     amigos.push(elementoHTML);    
+     actualizarLista();
+   
     }else{
         alert("Por favor, inserte un nombre.");
     }
-   
+}
+function actualizarLista(){
 
+    console.log('paso');
+    let nombreListA=document.getElementById('listaAmigos');
+    let nuevoNombre =document.createElement('li');
+    nuevoNombre.textContent=amigos[amigos.length-1];
+    nombreListA.appendChild(nuevoNombre);        
+     document.getElementById('amigo').value='';    
+    console.log('paso for'); 
 }
